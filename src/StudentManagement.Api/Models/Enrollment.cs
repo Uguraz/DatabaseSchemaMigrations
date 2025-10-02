335819306d3940101dffae6aaacd8b4cd830ae99
Uguraz <lassesyppli@yahoo.dk>
2025-10-02 14:00:28 +0200

    Add Course and Enrollment entities, DbContext config, migration and CoursesController

---
+++ src/StudentManagement.Api/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentManagement.Api.Models;
+
+public class Enrollment
+{
+    public int Id { get; set; }                 // PK (surrogate key)
+    public int StudentId { get; set; }          // FK -> Student
+    public int CourseId { get; set; }           // FK -> Course
+    public DateTime EnrolledAt { get; set; } = DateTime.UtcNow;
+    public int? Grade { get; set; }             // valgfrit felt (0-100)
+
+    // Navigation
+    public Student? Student { get; set; }
+    public Course? Course { get; set; }
+}
--- src/StudentManagement.Api/obj/Debug/net9.0/StudentManagement.Api.AssemblyInfo.cs
+++ src/StudentManagement.Api/obj/Debug/net9.0/StudentManagement.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagement.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03aa9b00ceff2dc4d195ad334e12687cef61978b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagement.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagement.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
